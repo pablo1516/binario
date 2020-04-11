cdbe3cb94f21a0651888b62c21165c2ab3fe5d36
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-04-10 19:12:02 -0500

    Add files via upload

---
+++ Binaria/src/binaria/Binaria.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binaria;
+
+/**
+ *
+ * @author david
+ */
+public class Binaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmBinaria frmbi = new FrmBinaria();
+        frmbi.setVisible(true);
+        frmbi.setLocationRelativeTo(null);
+        frmbi.setSize(400,300);
+        frmbi.setTitle("Busqueda Binaria");
+    }
+    
+}
